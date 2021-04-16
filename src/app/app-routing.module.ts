3a7866a05150224d0371f25c06464722e101cff3
Stefano Notaro Francesco <SNotaro@infragistics.com>
2021-04-16 16:10:32 -0300

    404 and rountings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IngredientsComponent } from './components/ingredients/ingredients.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'ingredients', component: IngredientsComponent },
   { path: 'recipes', component: RecipesComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'recipes' }
+  { path: '404', component: PageNotFoundComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'recipes' },
+  { path: '**', pathMatch: 'full', redirectTo: '404' }
 ];
 
 @NgModule({
