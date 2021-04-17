396ef3a58a65876e291c125313ed3f2028864c4a
Stefano Notaro Francesco <SNotaro@infragistics.com>
2021-04-17 20:18:59 -0300

    Ingredients units

--- src/app/shared/angular-material.module.ts
+++ src/app/shared/angular-material.module.ts
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 const angularMaterialModules = [
   MatSidenavModule,
@@ -14,6 +15,7 @@ const angularMaterialModules = [
   MatIconModule,
   MatDialogModule,
   MatInputModule,
+  MatSelectModule,
 ];
 
 @NgModule({
