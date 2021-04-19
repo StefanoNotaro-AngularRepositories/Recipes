96d1fa1f1d45e23d495c0912e59d57c367fab399
Stefano Notaro Francesco <SNotaro@infragistics.com>
2021-04-19 15:45:23 -0300

    Ingredients

--- src/app/components/ingredients/ingredients.component.ts
+++ src/app/components/ingredients/ingredients.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { LoginService } from '../../core/services/login.service';
 import { IngredientDialogComponent } from './dialogs/ingredient-dialog/ingredient-dialog.component';
+import { IngredientUnits } from './models/ingredient-units.interface';
 import { Ingredient } from './models/ingredient.interface';
+import { IngredientsUnitsService } from './services/ingredients-units.service';
 import { IngredientsService } from './services/ingredients.service';
 
 @Component({
@@ -11,11 +13,13 @@ import { IngredientsService } from './services/ingredients.service';
   styleUrls: ['./ingredients.component.scss']
 })
 export class IngredientsComponent implements OnInit {
+  public ingredientUnits: IngredientUnits[] = [];
   public ingredients: Ingredient[] = [];
   public isLogin = false;
 
   constructor(
     private ingredientsService: IngredientsService,
+    private ingredientsUnitsService: IngredientsUnitsService,
     public dialog: MatDialog,
     private loginService: LoginService) { }
 
@@ -24,7 +28,19 @@ export class IngredientsComponent implements OnInit {
       this.ingredients = ingredients;
     });
 
+    this.ingredientsUnitsService.get().subscribe(x => {
+      this.ingredientUnits = x;
+    });
+
     this.isLogin = this.loginService.getIsLogin();
+  }
+
+  public getIngredientUnitAbbreviation(ingredient: Ingredient): string {
+    return this.ingredientUnits.find(x => x.id === ingredient.unit)?.abbreviation ?? '';
+  }
+
+  public getIngredientUnitName(ingredient: Ingredient): string {
+    return this.ingredientUnits.find(x => x.id === ingredient.unit)?.name ?? '';
   }
 
   public openAddIngredientDialog(): void {
